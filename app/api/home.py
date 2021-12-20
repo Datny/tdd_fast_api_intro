556c3632a61325f36c3af58ee3abb6d708b93433
datny <robert.przydatek.cl@gmail.com>
2021-12-20 09:02:19 -0800

    add auto deployment to heroku

--- app/api/home.py
+++ app/api/home.py
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.get("/")
 async def home(settings: Settings = Depends(get_settings)):
     return {
-        "hello_santa": "Y",
+        "hello_santa": "Yes",
         "environment": settings.environment,
         "testing": settings.testing,
     }
--- tests/test_home.py
+++ tests/test_home.py
@@ -2,7 +2,7 @@ def test_home(test_app):
     response = test_app.get("/")
     assert response.status_code == 200
     assert response.json() == {
-        "hello_santa": "Y",
+        "hello_santa": "Yes",
         "environment": "dev",
         "testing": True,
     }
